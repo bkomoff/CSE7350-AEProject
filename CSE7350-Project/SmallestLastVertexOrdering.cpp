6535467933f27299dde2c570fae4fda31e106abc
Bryan <bjkomoff@gmail.com>
2019-04-28 13:09:38 -0400

    update

--- CSE7350-Project/SmallestLastVertexOrdering.cpp
+++ CSE7350-Project/SmallestLastVertexOrdering.cpp
@@ -8,11 +8,6 @@ SmallestLastVertexOrdering::SmallestLastVertexOrdering(int length)
 	numberOfSessions = length;
 
 	courses = new Vertex[numberOfSessions];
-
-	for (int i = 0; i < numberOfSessions; i++)
-	{
-		courses[i] = nullptr;
-	}
 }
 
 void SmallestLastVertexOrdering::CreateEdgeDegreeList(AdjacencyList const & currentSession)
